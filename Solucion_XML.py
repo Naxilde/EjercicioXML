97a978bd214534268652afecc166eec500e26654
Naxilde <aster9812@gmail.com>
2017-02-22 11:18:22 +0100

    Update Solucion_XML.py

--- Solucion_XML.py
+++ Solucion_XML.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from lxml import etree
 document = etree.parse("pokemon.xml")
-root = doc.getroot()
-poke = raiz.xpath("//pokedex/pokemon")
-for type in poke:
-  print type.find("type").text 
+root = document.getroot()
+poke = root.xpath("//pokedex/pokemon")
+
