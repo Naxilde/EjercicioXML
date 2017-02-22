d23bb7977d59bb85b15ec21c8d00e25f51f016b9
Naxilde <aster9812@gmail.com>
2017-02-22 14:26:37 +0100

    Update Solucion_XML.py
    Añadido ejercicio 2

--- Solucion_XML.py
+++ Solucion_XML.py
@@ -3,8 +3,13 @@ from lxml import etree
 document = etree.parse("pokemon.xml")
 root = document.getroot()
 poke = root.xpath("//pokedex/pokemon")
-tipo = root.findall("pokemon/type")
 
+tipo = root.findall("pokemon/type")
+tipo_traduccion = []
+for t in tipo:
+  tipo_traduccion.append(t.text)
+
+# Ejercicio 1
 nombres = []
 for l in tipo:
   if l.getparent().find("name").text not in nombres:
@@ -12,9 +17,11 @@ for l in tipo:
     nombres.append(l.getparent().find("name").text)
   print l.text
 
+# Ejercicio 2
 tipos = []
-for c in tipo:
-  contador = 0
-  if c.text not in tipos:
-    print "\n", c.text
-    tipos.append(c.text)
+for c in tipo_traduccion:
+  if c not in tipos:
+    print c, tipo_traduccion.count(c)
+    tipos.append(c)
+    
+# Ejercicio 3
