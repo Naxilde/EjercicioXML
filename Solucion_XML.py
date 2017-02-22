8a645bbe0f3d0ce976baeb5878ac118ac3eaf3c5
Naxilde <aster9812@gmail.com>
2017-02-22 13:39:52 +0100

    Update Solucion_XML.py

--- Solucion_XML.py
+++ Solucion_XML.py
@@ -3,9 +3,8 @@ from lxml import etree
 document = etree.parse("pokemon.xml")
 root = document.getroot()
 poke = root.xpath("//pokedex/pokemon")
-
 tipo = root.findall("pokemon/type")
-for i in tipo:
-  if i.getparent().find("nombre").text
-  print i.getparent().find("name").text
-  print i.text
+
+
+for l in tipo:
+  print l.getparent().find("name").text, l.text, "\n"
