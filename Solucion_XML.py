a0b43aef2925c194f37035d50d476dd08f38fd33
Naxilde <aster9812@gmail.com>
2017-02-22 13:58:58 +0100

    Update Solucion_XML.py

--- Solucion_XML.py
+++ Solucion_XML.py
@@ -5,10 +5,16 @@ root = document.getroot()
 poke = root.xpath("//pokedex/pokemon")
 tipo = root.findall("pokemon/type")
 
-# Ejercicio 1
-lista = []
+nombres = []
 for l in tipo:
-  if l.getparent().find("name").text not in lista:
+  if l.getparent().find("name").text not in nombres:
     print "\n", l.getparent().find("name").text
-    lista.append(l.getparent().find("name").text)
+    nombres.append(l.getparent().find("name").text)
   print l.text
+
+tipos = []
+for c in tipo:
+  contador = 0
+  if c.text not in tipos:
+    print "\n", c.text
+    tipos.append(c.text)
